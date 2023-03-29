4b45b21db5f90d42b57264b4a0e1d0f8d10e2dd7
Carlos Huallpa <hualpacarlos32@gmail.com>
2023-03-29 18:45:46 -0300

    tipos de variebles

--- Variables.js
+++ Variables.js
@@ -1,1 +1,25 @@
 var cajon ;
+
+// string
+var bootcamp = "henry"
+
+// numeros
+ var numerosEntero = 26
+
+ var numerosNegativo = -4
+
+ var numerosDecial = 11.75
+
+ // booleanos
+
+ var estoyEntendiendo = true
+
+ var estoyAburrido = false
+
+ // undefined 
+ var cajon1
+
+ console.log(cajon1);
+
+ // null
+ var sinNada = null
